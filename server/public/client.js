e402f749af6d163a8c5f84dc45eb1e53fe75945a
Marifel Angeles <mmarifel@gmail.com>
2019-02-07 16:39:36 -0600

    post new player on server

--- server/public/client.js
+++ server/public/client.js
@@ -4,5 +4,38 @@ $(document).ready(docReady);
 
 function docReady() {
     console.log('jq');
-    
+    // when add player is clicked
+    $('#addPlayerButton').on('click', handleAddNewPlayer);
 }// end docReady
+
+function handleAddNewPlayer(){
+    console.log('addNewPlayer is clicked');
+    // target input field val
+    console.log($('#newPlayerInput').val());
+    
+    // post new player on server
+    $.ajax({
+        url: '/players',
+        method: 'POST',
+        data: {
+            name: $('#newPlayerInput').val() 
+        }
+    }).then( function(){
+        // get new player from server
+        getNewPlayer();
+    });
+
+}// end addNewPlayer
+
+function getNewPlayer() {
+    console.log('in getNewPlayer');
+    
+    // make get request
+    $.ajax({
+        url: '/players',
+        method: 'GET'
+    }).then(function(response){
+        console.log('response', response);
+        
+    })
+} // end getNewPlayer
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 let PORT = 5000;
 
@@ -8,10 +9,17 @@ app.listen(PORT, () => {
 });
 
 app.use(express.static('server/public') );
+app.use(bodyParser.urlencoded({extended: true} ) );
 
+//store players data in server
 let players = require('./module/players')
-
 app.get('/players', (req, res) => {
     res.send(players);    
+});
+
+//post new player in server
+app.post('/players', (req, res) => {
+    players.push(req.body.name);
+    res.sendStatus(201);
 });
 
